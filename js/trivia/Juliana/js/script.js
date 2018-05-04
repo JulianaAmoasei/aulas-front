3f1b59eb07e398d5bf93ad0d385b96b4691460b3
fukui <r.fukui@gmail.com>
2018-05-04 01:14:16 -0300

    pequenas modificacoes de css

--- js/trivia/Juliana/js/script.js
+++ js/trivia/Juliana/js/script.js
@@ -1,7 +1,7 @@
 
 var nome = prompt("digite seu nome");
 
-if (nome !== null) {
+if (nome != null) {
 
     document.getElementById("promptNome").innerHTML = 
         "Bem-vinda, " + nome + "!";
@@ -27,25 +27,25 @@ if (iniciaJogo == "não") {
 
 if (pergunta1 == "patolino") {
     var acertou1 = document.getElementById("resultadosCertos");
-    acertou1.innerHTML += "<p>PATOLINO, resposta correta!</p>";
+    acertou1.innerHTML += "<span>PATOLINO, resposta correta!</span>";
 } else {
     var errou1 = document.getElementById("resultadosErrados");
-    errou1.innerHTML += "<p>Errado. O correto é PATOLINO.</p>";
+    errou1.innerHTML += "<span>Errado. O correto é PATOLINO.</span>";
 }
 
 if (pergunta2 == "chiquinha") {
     var acertou2 = document.getElementById("resultadosCertos");
-    acertou2.innerHTML += "<p>CHIQUINHA, resposta correta!</p>";
+    acertou2.innerHTML += "<span>CHIQUINHA, resposta correta!</span>";
 } else {
     var errou2 = document.getElementById("resultadosErrados");
-    errou2.innerHTML += "<p>Errado. O correto é CHIQUINHA.</p>";
+    errou2.innerHTML += "<span>Errado. O correto é CHIQUINHA.</span>";
 }
 
 if (pergunta3 == "azul") {
     var acertou3 = document.getElementById("resultadosCertos");
-    acertou3.innerHTML += "<p>AZUL, resposta correta!</p>";
+    acertou3.innerHTML += "<span>AZUL, resposta correta!</span>";
 } else {
     var errou3 = document.getElementById("resultadosErrados");
-    errou3.innerHTML += "<p>Errado. O correto é AZUL.</p>";
+    errou3.innerHTML += "<span>Errado. O correto é AZUL.</span>";
 }
 
