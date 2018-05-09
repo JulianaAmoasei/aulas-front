0f739fc0b7d42dbc9544e2b1aa1f619cf6f2af98
JulianaAmoasei <late.parrot@gmail.com>
2018-05-09 15:15:57 -0300

    mini-exercicios javascripting

---
+++ js/basicos-aula00/script.js
@@ -0,0 +1,8 @@
+
+var nota1 = parseFloat(prompt("insira nota 1"));
+var nota2 = parseFloat(prompt("insira nota 2"));
+var nota3 = parseFloat(prompt("insira nota 3"));
+
+var notasSomadas = (nota1 + nota2 + nota3) / 3;
+
+document.write(Math.round(notasSomadas));
